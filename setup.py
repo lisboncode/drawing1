f2ed92f35a7060b0bb520b6a923db09d9a0e3b66
lisboncode <lisboncode@gmail.com>
2024-05-09 14:31:48 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='drawing1',
+      version='1.0',
+      description='Drawing1 Distribution Package',
+      author='SHR',
+      author_email='Sergio.rodrigues@ipt.pt',
+      url='The URL project - github for example',
+      packages=['drawing1'],
+     )
+
+
+
+
+
+
+
